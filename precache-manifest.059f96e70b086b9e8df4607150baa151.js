26e11201916b8a1715d5493b4b2e9e5c2973096f
MohammedAl-rowad <rowadwo@gmail.com>
2020-05-27 05:59:53 +0300

    deploy second version

---
+++ precache-manifest.059f96e70b086b9e8df4607150baa151.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49e181116d0fdc42aa4f1d8b62b2d405",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dab31ba8c01c71c137e1",
+    "url": "/static/css/2.90405288.chunk.css"
+  },
+  {
+    "revision": "be1b5b4b77711fb3bc2c",
+    "url": "/static/css/main.cbbf0a7c.chunk.css"
+  },
+  {
+    "revision": "dab31ba8c01c71c137e1",
+    "url": "/static/js/2.12e7a12b.chunk.js"
+  },
+  {
+    "revision": "0e9dd95e584768c812feb507ac3388cc",
+    "url": "/static/js/2.12e7a12b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be1b5b4b77711fb3bc2c",
+    "url": "/static/js/main.7e1762d2.chunk.js"
+  },
+  {
+    "revision": "758d543d3c3c62d8d5a7",
+    "url": "/static/js/runtime-main.40600b02.js"
+  }
+]);
