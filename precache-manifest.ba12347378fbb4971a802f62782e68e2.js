8ded1c9388c77a69c8e55034877bfe4f6dbc5869
MohammedAl-rowad <rowadwo@gmail.com>
2020-06-17 15:46:47 +0300

    fix path to images

--- precache-manifest.ba12347378fbb4971a802f62782e68e2.js
+++ precache-manifest.ba12347378fbb4971a802f62782e68e2.js
@@ -1,46 +1,46 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f8ad3d4d4292a5709fa3d2ddd427c3f6",
-    "url": "/index.html"
+    revision: 'f8ad3d4d4292a5709fa3d2ddd427c3f6',
+    url: '/index.html',
   },
   {
-    "revision": "2ab45d6c0fc9f0b42041",
-    "url": "/static/css/2.90405288.chunk.css"
+    revision: '2ab45d6c0fc9f0b42041',
+    url: '/static/css/2.90405288.chunk.css',
   },
   {
-    "revision": "5c1ce654050044be7977",
-    "url": "/static/css/main.d88a19cc.chunk.css"
+    revision: '5c1ce654050044be7977',
+    url: '/static/css/main.d88a19cc.chunk.css',
   },
   {
-    "revision": "2ab45d6c0fc9f0b42041",
-    "url": "/static/js/2.138e4dcc.chunk.js"
+    revision: '2ab45d6c0fc9f0b42041',
+    url: '/static/js/2.138e4dcc.chunk.js',
   },
   {
-    "revision": "0e9dd95e584768c812feb507ac3388cc",
-    "url": "/static/js/2.138e4dcc.chunk.js.LICENSE.txt"
+    revision: '0e9dd95e584768c812feb507ac3388cc',
+    url: '/static/js/2.138e4dcc.chunk.js.LICENSE.txt',
   },
   {
-    "revision": "5c1ce654050044be7977",
-    "url": "/static/js/main.829c5d57.chunk.js"
+    revision: '5c1ce654050044be7977',
+    url: '/static/js/main.829c5d57.chunk.js',
   },
   {
-    "revision": "758d543d3c3c62d8d5a7",
-    "url": "/static/js/runtime-main.40600b02.js"
+    revision: '758d543d3c3c62d8d5a7',
+    url: '/static/js/runtime-main.40600b02.js',
   },
   {
-    "revision": "759e3b4df363fda997e161b3d0e26053",
-    "url": "/static/media/chart2.759e3b4d.svg"
+    revision: '759e3b4df363fda997e161b3d0e26053',
+    url: '/vexpest_V2/static/media/chart2.759e3b4d.svg',
   },
   {
-    "revision": "9312511d104ce3bdd260b38ba3ea9bde",
-    "url": "/static/media/creator.9312511d.svg"
+    revision: '9312511d104ce3bdd260b38ba3ea9bde',
+    url: '/vexpest_V2/static/media/creator.9312511d.svg',
   },
   {
-    "revision": "62754a97bd86f1f7d5869a8fde33471a",
-    "url": "/static/media/sourceCode.62754a97.svg"
+    revision: '62754a97bd86f1f7d5869a8fde33471a',
+    url: '/vexpest_V2/static/media/sourceCode.62754a97.svg',
   },
   {
-    "revision": "23ee41cea8206db1ffa40247bf989e51",
-    "url": "/static/media/spaceman.23ee41ce.svg"
-  }
-]);
+    revision: '23ee41cea8206db1ffa40247bf989e51',
+    url: '/vexpest_V2/static/media/spaceman.23ee41ce.svg',
+  },
+])
