c0d2de0e502af0915fa7f161e63ec1eaed01d1c1
MohammedAl-rowad <rowadwo@gmail.com>
2020-06-25 16:14:11 +0300

    deploy - savetheme - fix route

---
+++ precache-manifest.fea96e2601d8aae06b9aab3aff879709.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73823365e7f668afb951b314b800d831",
+    "url": "/vexpest_V2/index.html"
+  },
+  {
+    "revision": "d51d41099cc49a33d921",
+    "url": "/vexpest_V2/static/css/2.90405288.chunk.css"
+  },
+  {
+    "revision": "7e995073132301673276",
+    "url": "/vexpest_V2/static/css/main.d88a19cc.chunk.css"
+  },
+  {
+    "revision": "d51d41099cc49a33d921",
+    "url": "/vexpest_V2/static/js/2.e901f348.chunk.js"
+  },
+  {
+    "revision": "0e9dd95e584768c812feb507ac3388cc",
+    "url": "/vexpest_V2/static/js/2.e901f348.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e995073132301673276",
+    "url": "/vexpest_V2/static/js/main.41eebe55.chunk.js"
+  },
+  {
+    "revision": "abf6dd753075f305fdfa",
+    "url": "/vexpest_V2/static/js/runtime-main.4c3f502c.js"
+  },
+  {
+    "revision": "759e3b4df363fda997e161b3d0e26053",
+    "url": "/vexpest_V2/static/media/chart2.759e3b4d.svg"
+  },
+  {
+    "revision": "9312511d104ce3bdd260b38ba3ea9bde",
+    "url": "/vexpest_V2/static/media/creator.9312511d.svg"
+  },
+  {
+    "revision": "62754a97bd86f1f7d5869a8fde33471a",
+    "url": "/vexpest_V2/static/media/sourceCode.62754a97.svg"
+  },
+  {
+    "revision": "23ee41cea8206db1ffa40247bf989e51",
+    "url": "/vexpest_V2/static/media/spaceman.23ee41ce.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ba12347378fbb4971a802f62782e68e2.js"
+  "/vexpest_V2/precache-manifest.fea96e2601d8aae06b9aab3aff879709.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vexpest_V2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
